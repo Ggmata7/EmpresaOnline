41b486853dc79d5234e84c2bac38616eaed81743
Ggmata7 <gabrielantonio33@gmail.com>
2026-09-04 14:49:06 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,14 @@
 import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**', // Permite carregar imagens de qualquer CDN de marketplace
+      },
+    ],
+  },
+};
 
 export default nextConfig;
